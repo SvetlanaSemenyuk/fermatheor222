f85e87d2d1433779f3b9cad4857173df1d96eb37
nanites-club <Semenyuk1899@mail.ru>
2018-10-01 12:32:37 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,25 @@
+package com.company;
+import java.util.Scanner;
+public class Main {
+
+    public static <string> void main(String[] args) {
+       // Scanner scan = new Scanner(System.in);
+        //int n=scan.nextInt();
+        double c=0, d=0;
+           for( int n=2; n<=5; n++){
+           // for (int  c = 1; c<=100; c++){
+                for (int a = 1; a <=10; a++)
+                    for (int b = 1; b <= 10; b++){
+                    //c=(Math.pow(a, n));
+                        d=(Math.pow(a, n)+Math.pow(b,n));
+                        c=Math.pow(d,1/n);
+                            System.out.println("a^n+b^n="+Math.pow(c, n));
+                        System.out.println("a^n+d^n="+d);
+                        System.out.println("d,1/n"+c);
+            }}}}
+        /*String s1="stroka1", s2="stroka2", s3="stroka3", s4="stroka4", s5="stroka5";
+        if (s4==s5){
+            System.out.println(s1+s2);
+        }
+        else
+            System.out.println(s1 + s3);*/
